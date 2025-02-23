c0da7726e7811f4d46f6474fcad823f4b0638f1d
Drihmia <drihmia.redouane@gmail.com>
2025-02-23 17:09:34 +0000

    perf: Improve AI model performance by using different AI model at a time if the Quota of one of them is run out automatically pass the next model

--- tools/gemini_chat.py
+++ tools/gemini_chat.py
@@ -15,14 +15,14 @@ if not api_key_gimini:
     print("Please set the GEMINI_API_KEY environment variable.")
     sys.exit(1)
 
-def gemini_model(system_messages: dict, api_key: str) -> genai.GenerativeModel:
+def gemini_model(system_messages: dict, api_key: str, model_name) -> genai.GenerativeModel:
     """
     Create a Gemini model
     """
 
     client_gemini = genai.configure(api_key=api_key)
     model=genai.GenerativeModel(
-        model_name="gemini-2.0-flash-thinking-exp-01-21",
+        model_name="gemini-2.0-flash-exp",
         system_instruction=system_messages.get("parts"),
         generation_config={
             "top_p": 0.98,
@@ -34,15 +34,35 @@ def gemini_model(system_messages: dict, api_key: str) -> genai.GenerativeModel:
     return model
 
 # Create the Gemini model
-_gemini_model = gemini_model(system_prompt, api_key_gimini)
+_gemini_model_send_message = gemini_model(system_prompt, api_key_gimini, "gemini-2.0-flash-exp")
 
-def chat_gemini(system_messages: dict) -> str:
+_gemini_model_generate_content = gemini_model(system_prompt, api_key_gimini, "gemini-2.0-flash-thinking-exp-01-21")
+
+def chat_gemini_send_message(system_messages: dict, message: str) -> str:
     """
     Chat with the Gemini API
     """
 
-    response = _gemini_model.generate_content(system_messages).text.strip()
-    print("+"*200)
-    print(response.removeprefix("```html").removesuffix("```").strip())
-    print("+"*200)
-    return '\n' + response.removeprefix("```html").removesuffix("```").strip()
+    # response = _gemini_model.generate_content(system_messages).text.strip()
+    try:
+        chat_session = _gemini_model_send_message.start_chat(history=system_messages)
+        response = chat_session.send_message(message).text.strip()
+        response = response.removeprefix("```html").removesuffix("```").strip()
+        return '\n' + response
+    except Exception as e:
+        print("Error creating the Gemini model:", e)
+    return '\n'
+
+def chat_gemini_generate_content(system_messages: dict) -> str:
+    """
+    Chat with the Gemini API
+    """
+
+
+    try:
+        response = _gemini_model_generate_content.generate_content(system_messages).text.strip()
+        response = response.removeprefix("```html").removesuffix("```").strip()
+        return '\n' + response
+    except Exception as e:
+        print("Error creating the Gemini model:", e)
+    return '\n'
