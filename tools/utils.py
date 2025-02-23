6a6078be619ccf31fa7eac5e00ce4888e020685c
Drihmia <drihmia.redouane@gmail.com>
2025-02-23 17:03:06 +0000

    feat: Add necessay tools for time and date functions for appending time at the end of each message

--- tools/utils.py
+++ tools/utils.py
@@ -1,5 +1,9 @@
 import os
 import json
+from zoneinfo import ZoneInfo
+from datetime import datetime
+from tools.chat_utils import update_time_in_time_tag
+
 
 
 # Function to load chat histories for a specific user
@@ -49,10 +53,65 @@ def load_chat_history_startup(user_chat_histories, history_dir="chat_histories")
                 # print("loading chat history for user:", user_id)
                 user_chat_histories[user_id] = load_chat_history(user_id, history_dir=history_dir)
 
-    print("Number of chat histories loaded:", len(user_chat_histories))
-    for chat in user_chat_histories:
-        print(f"User {chat} has {len(user_chat_histories[chat])} messages in their chat history.")
+    print("Number of chat histories loaded:", len(user_chat_histories), "*"*30)
+    # for chat in user_chat_histories:
+        # print(f"User {chat} has {len(user_chat_histories[chat])} messages in their chat history.")
 
     return user_chat_histories
 
 
+def get_time_zone_name(time_zone):
+    """
+    Get the time zone name from the time zone key
+    Args: time_zone: The time zone key
+    Return: The time zone name
+    """
+
+    try:
+        return ZoneInfo(time_zone)
+    except Exception as e:
+        print("get_time_zone_name",e)
+        return "UTC"
+
+def get_current_time(time_zone):
+    """
+    Get the current time in the specified time zone
+    Args: time_zone: The time zone key
+    Return: The current time in the specified time zone
+    """
+
+    try:
+
+        hold = get_time_zone_name(time_zone)
+        return datetime.now(hold).strftime("%Y-%m-%d %H:%M:%S")
+    except Exception as e:
+        print("get_current_time", e)
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def append_current_time(role, message):
+    """
+    append the current time to the message to:
+        user: user messsage will be included in a <p> tag and the time will be included in a <time> tag
+        AI Model: AI Model HTML message is html already so the time will be included in a <time> tag
+    Args: role: The role of the message sender
+          message: The message to append the current time to it
+    Return: The message with the current time appended to it
+    """
+
+    current_time = get_current_time("Africa/Casablanca")
+    if role.lower() == "user":
+        if ('<time>' in message) or ('<time ' in message):
+            return update_time_in_time_tag(message, current_time)
+        return update_time_in_time_tag(f'<p>{message}</p><br><time>{current_time}</time>', current_time)
+    else:
+        if ('<time>' in message) or ('<time ' in message):
+            return update_time_in_time_tag(message, current_time)
+        print("append_current_time", message)
+        return update_time_in_time_tag(f'{message}<br><time>{current_time}</time>', current_time)
+
+
+
+if __name__ == "__main__":
+    # print("This is a utility module. It is not meant to be executed directly.")
+    # print("get_time_zone_name", "Africa/Casablanca:", get_time_zone_name("Africa/Casablanca"))
+    print("get_current_time", "Africa/Casablanca:", get_current_time("Africa/Casablanca"))
